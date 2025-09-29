89fe13df2aa183e06e8d092369755736a92fb9d4
IanFrunze <ian.frunze06@gmail.com>
2025-09-29 18:04:53 +0100

    Refactor Board class to consolidate properties, enhance piece management, and improve validation logic add iterator

--- src/main/kotlin/pt/isel/Board.kt
+++ src/main/kotlin/pt/isel/Board.kt
@@ -1,5 +1,7 @@
 package pt.isel
 
+import kotlin.collections.find
+
 
 const val SIDE_MIN = 4
 const val SIDE_MAX = 26
@@ -7,104 +9,95 @@ const val SIDE_MAX = 26
 /**
  * Represents a board game grid.
  *
- * @property Piece The piece on the board, either 'b' for black or 'w' for white.
- * @property rows The number of rows on the board.
- * @property cols The number of columns on the board.
+ * @property pieces The list of pieces on the board.
+ * @property side The size of the board (side x side).
  */
-data class Board(private val rows: Int,
-                private val cols: Int,
-                private val  pieces: List<Piece> = emptyList()
-) {
 
-    constructor(row: Int) : this(row, row)
+@ConsistentCopyVisibility
+data class Board private constructor(
+    private val side: Int,
+    private val pieces: List<Piece>
+): Iterable<Char> {
+
+    constructor(side: Int) : this(side = side, emptyList())
 
     init {
-        require(rows in SIDE_MIN..SIDE_MAX) {
-            "Row must be between $SIDE_MIN and $SIDE_MAX"
+        require(side in SIDE_MIN..SIDE_MAX) {
+            "Side must be between $SIDE_MIN and $SIDE_MAX"
         }
-        require(cols in SIDE_MIN..SIDE_MAX) {
-            "Column must be between $SIDE_MIN and $SIDE_MAX"
-        }
-        require(rows % 2 == 0 && cols % 2 == 0) {
-            "Row must be even"
+        require(side % 2 == 0) {
+            "Side must be even"
         }
     }
 
     /**
      * Represents a piece on the board.
      */
-    data class Piece(val row: Int, val col: Int, var value: Char) {
-        init {
-            val char = value.lowercase()[0]
-            require(row in 1..SIDE_MAX) {
-                "Row must be between 1 and $SIDE_MAX"
-            }
-            require(col in 1..SIDE_MAX) {
-                "Column must be between 1 and $SIDE_MAX"
-            }
-            require(char == 'b' || char == 'w') {
-                "Value must be 'b' or 'w'"
-            }
-        }
-    }
+    private data class Piece(val row: Int, val col: Int, val value: Char)
 
     /**
      * Gets the piece at the specified row and column.
      * @return The piece at the specified position, or null if there is no piece.
      */
-    operator fun get(row: Int, col: Char): Piece? = this[row, charIndexToInt(col)]
+    operator fun get(row: Int, col: Char): Char? = this[row, col.toIntIndex()]
 
     /**
      * Gets the piece at the specified row and column.
      * @return The piece at the specified position, or null if there is no piece.
      */
-    operator fun get(row: Int, col: Int): Piece? {
-        require(row in 1..rows) {
-            "Row must be between 1 and $rows" }
-        require(col in 1..cols) {
-            "Column must be between 1 and $cols"
+    operator fun get(row: Int, col: Int): Char? {
+        require(row in 1..side) {
+            "Row must be between 1 and $side" }
+        require(col in 1..side) {
+            "Column must be between 1 and $side"
         }
-        return pieces.find { it.row == row && it.col == col }
+        return pieces.find { it.row == row && it.col == col }?.value
     }
 
     /**
      * Changes the piece at the specified row and column from 'b' to 'w' or from 'w' to 'b'.
      * @return true if the piece was changed, false if there is no piece at the specified position.
      */
-    fun changePiece(row: Int, col: Char): Boolean = changePiece(row, charIndexToInt(col))
+    fun changePiece(row: Int, col: Char): Board =
+        changePiece(row, col.toIntIndex())
 
     /**
      * Changes the piece at the specified row and column from 'b' to 'w' or from 'w' to 'b'.
      * @return true if the piece was changed, false if there is no piece at the specified position.
      */
-    fun changePiece(row: Int, col: Int): Boolean {
-        require(row in 1..rows) {
-            "Row must be between 1 and $rows"
+    fun changePiece(row: Int, col: Int): Board {
+        require(row in 1..side) {
+            "Row must be between 1 and $side"
         }
-        require(col in 1..cols) {
-            "Column must be between 1 and $cols}"
+        require(col in 1..side) {
+            "Column must be between 1 and $side}"
         }
-        val value = this[row,col]?.value ?: return false
+        val value = this[row,col] ?: return this
         val newValue = if (value == 'b') 'w' else 'b'
-        this[row,col]?.value = newValue
-        return true
+        return this.copy(pieces = pieces.map {piece ->
+            if (piece.row == row && piece.col == col)
+                piece.copy(value = newValue)
+            else
+                piece
+        })
     }
 
     /**
      * Adds a piece to the board at the specified row and column.
      */
-    fun addPiece(row: Int, col: Char, value: Char): Board = this.addPiece(row, charIndexToInt(col), value)
+    fun addPiece(row: Int, col: Char, value: Char): Board =
+        this.addPiece(row, col.toIntIndex(), value)
 
     /**
      * Adds a piece to the board at the specified row and column.
      */
     fun addPiece(row: Int, col: Int, value: Char): Board {
         val value = value.lowercase()[0]
-        require(row in 1..rows) {
-            "Row must be between 1 and $rows"
+        require(row in 1..side) {
+            "Row must be between 1 and $side"
         }
-        require(col in 1..cols) {
-            "Column must be between 1 and $cols"
+        require(col in 1..side) {
+            "Column must be between 1 and $side"
         }
         require(value == 'b' || value == 'w') {
             "Value must be 'b' or 'w'"
@@ -116,10 +109,10 @@ data class Board(private val rows: Int,
     /**
      * Converts a column character ('a', 'b', ...) to its corresponding integer index.
      */
-    private fun charIndexToInt(col: Char): Int {
-        val colLower = col.lowercase()[0]
-        require(colLower in 'a'..'a' + cols - 1) {
-            "Column must be between 'a' and '${'a' + cols - 1}'" }
+    private fun Char.toIntIndex(): Int {
+        val colLower = this.lowercase()[0]
+        require(colLower in 'a'..'a' + side - 1) {
+            "Column must be between 'a' and '${'a' + side - 1}'" }
         return colLower - 'a' + 1
     }
 
@@ -128,13 +121,21 @@ data class Board(private val rows: Int,
      * @return A list of the initial pieces.
      */
     fun startPieces(): Board {
-        val midRow = rows / 2
-        val midCol = cols / 2
+        val mid = side / 2
         return this.copy(pieces = listOf(
-            Piece(midRow, midCol, 'w'),
-            Piece(midRow + 1, midCol + 1, 'w'),
-            Piece(midRow, midCol + 1, 'b'),
-            Piece(midRow + 1, midCol, 'b')
+            Piece(row = mid, col = mid, value = 'w'),
+            Piece(row = mid + 1, col = mid + 1, value = 'w'),
+            Piece(row = mid, col = mid + 1, value = 'b'),
+            Piece(row = mid + 1, col = mid, value = 'b')
         ))
+    }
+
+    /**
+     * Returns an iterator of the value of pieces on the board.
+     */
+    override fun iterator(): Iterator<Char> = object : Iterator<Char> {
+        private val it = pieces.iterator()
+        override fun hasNext() = it.hasNext()
+        override fun next() = it.next().value
     }
 }
