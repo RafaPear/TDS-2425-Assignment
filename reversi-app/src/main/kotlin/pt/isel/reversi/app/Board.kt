0b3a1eb81af927ce5e1d8add483452b751d7fe3b
ianfrunze <ian.frunze06@gmail.com>
2025-11-09 13:15:01 +0000

    Add initial implementation of Board component with grid rendering

---
+++ reversi-app/src/main/kotlin/pt/isel/reversi/app/Board.kt
@@ -0,0 +1,81 @@
+package pt.isel.reversi.app
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+
+val PURPLE = Color(0xFF800080)
+val GRID_SIZE = 500.dp
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+    ) {
+        Board()
+    }
+}
+
+
+@Preview
+@Composable
+fun Board() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center,
+    ) { Grid() }
+}
+
+@Composable
+fun Grid() {
+    Box(
+        modifier = Modifier
+            .background(color = PURPLE)
+            .size(GRID_SIZE),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Column(
+            modifier = Modifier
+                .size(GRID_SIZE),
+        ) {
+            DrawGrid(8)
+        }
+    }
+}
+
+@Composable
+fun DrawGrid(side: Int) = Canvas(modifier = Modifier.size(GRID_SIZE)) {
+    val padding = (GRID_SIZE / side).toPx()
+
+    for (x in 1 until side) {
+        drawLine(
+            color = Color.Black,
+            strokeWidth = 4f,
+            start = Offset(x = x * padding, y = 0f),
+            end = Offset(x = x * padding, y = size.height),
+        )
+    }
+
+    for (y in 1 until side) {
+        drawLine(
+            color = Color.Black,
+            strokeWidth = 4f,
+            start = Offset(x = 0f, y = y * padding),
+            end = Offset(x = size.width, y = y * padding),
+        )
+    }
+}
