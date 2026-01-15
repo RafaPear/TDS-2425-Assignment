698b8cdeb93cc80c79a836f6b929c2edddff5cd2
Tito Silva <martimramos5000@gmail.com>
2026-01-15 18:39:54 +0000

    winner page view and viewModel push

---
+++ reversi-app/src/main/kotlin/pt/isel/reversi/app/pages/winnerPage/WinnerPageViewModel.kt
@@ -0,0 +1,35 @@
+package pt.isel.reversi.app.pages.winnerPage
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import pt.isel.reversi.app.state.pages.ScreenState
+import pt.isel.reversi.app.state.pages.UiState
+import pt.isel.reversi.app.state.pages.ViewModel
+import pt.isel.reversi.core.Game
+import pt.isel.reversi.core.Player
+import pt.isel.reversi.core.exceptions.ReversiException
+
+data class WinnerUiState(
+    override val screenState: ScreenState = ScreenState(),
+    val winner: Player? = null,
+    val gameName: String? = null,
+) : UiState {
+    override fun updateScreenState(newScreenState: ScreenState): UiState {
+        return copy(screenState = newScreenState)
+    }
+}
+
+class WinnerPageViewModel(
+    val game: Game,
+    override val globalError: ReversiException?,
+    override val setGlobalError: (Exception?) -> Unit
+) : ViewModel<WinnerUiState>() {
+    override val _uiState = mutableStateOf(
+        WinnerUiState(
+            winner = game.gameState?.winner,
+            gameName = game.currGameName,
+            screenState = ScreenState(error = globalError)
+        )
+    )
+    override val uiState: State<WinnerUiState> = _uiState
+}
---
+++ reversi-app/src/test/kotlin/pt/isel/reversi/app/WinnerPageTests.kt
@@ -0,0 +1,36 @@
+package pt.isel.reversi.app
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.runComposeUiTest
+import kotlinx.coroutines.runBlocking
+import pt.isel.reversi.app.pages.Page
+import pt.isel.reversi.app.pages.PagesState
+import pt.isel.reversi.app.state.AppState
+import pt.isel.reversi.core.Player
+import pt.isel.reversi.core.board.PieceType
+import pt.isel.reversi.core.startNewGame
+import pt.isel.reversi.core.storage.MatchPlayers
+import kotlin.test.Test
+
+@OptIn(ExperimentalTestApi::class)
+class WinnerPageTests {
+
+    val game = runBlocking {
+        startNewGame(
+            side = 4,
+            players = MatchPlayers(Player(type = PieceType.BLACK), Player(type = PieceType.WHITE)),
+            firstTurn = PieceType.BLACK,
+            currGameName = null
+        )
+    }
+
+    val appState = AppState.empty().copy(
+        game = game,
+        pagesState = PagesState(Page.GAME, Page.NONE)
+    )
+
+    @Test
+    fun `check winner page shows correct winner`() = runComposeUiTest {
+
+    }
+}
